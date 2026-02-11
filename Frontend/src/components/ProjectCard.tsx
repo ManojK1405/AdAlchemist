527fe771c2f53a8a6e29bdf2362a0780c983d43e
ManojK1405 <manojkalasgonda7@gmail.com>
2026-02-11 18:35:50 +0530

    feat: enhance MyGenerations page with loading state and project listing

--- Frontend/src/components/ProjectCard.tsx
+++ Frontend/src/components/ProjectCard.tsx
@@ -1,48 +1,218 @@
 import type React from "react"
-import type { Project } from "../Types";
-import { useNavigate } from "react-router-dom";
-import { useState } from "react";
-import { Loader2Icon } from "lucide-react";
+import type { Project } from "../Types"
+import { useNavigate } from "react-router-dom"
+import { useState } from "react"
+import { EllipsisIcon, ImageIcon, Loader2Icon, Share2Icon, Trash2Icon, VideoIcon } from "lucide-react"
+import { GhostButton, PrimaryButton } from "./Buttons"
 
-const ProjectCard = ({gen,setGenerations,forCommunity=false}:{gen:Project,setGenerations:React.Dispatch<React.SetStateAction<Project[]>>,forCommunity?:boolean}) => {
 
-    const navigate = useNavigate();
-    const [menuOpen,setMenuOpen] = useState(false);
+const ProjectCard = ({
+  gen,
+  setGenerations,
+  forCommunity = false
+}: {
+  gen: Project
+  setGenerations: React.Dispatch<React.SetStateAction<Project[]>>
+  forCommunity?: boolean
+}) => {
 
+  const navigate = useNavigate()
+  const [menuOpen, setMenuOpen] = useState(false)
+
+  const handleDelete = async (id: string)=>{
+    const confirm = window.confirm('Are you sure you want to delete this project?');
+    if(!confirm) return;
+    console.log(id)
+  }
+
+  const togglePublish = async (projectId: string)=>{
+    console.log(projectId)
+  }
 
   return (
-
     <div key={gen.id} className="mb-4 break-inside-avoid">
-    <div className="bg-white/5 border border-white/10 rounded-x1 overflow-hidden hover:border-white/20 transition group">
+      <div className="bg-white/5 border border-white/10 rounded-x1 overflow-hidden hover:border-white/20 transition group">
 
-        {/*preview image*/}
-        <div className={`${gen?.aspectRatio === '9:16' ? 'aspect-9/16' : 'aspect-video'} relative overflow-hidden`}>
-            {gen.generatedImage && (
-            <img src={gen.generatedImage} alt={gen.productName} className={`absolute inset-0 w-full h-full object-cover transition duration-500 ${gen.generatedVideo ? 'group-hover:opacity-0' :'group-hover: scale-105'}`} />
+        {/* preview image */}
+        <div className={`${gen?.aspectRatio === '9:16' ? 'aspect-[9/16]' : 'aspect-video'} relative overflow-hidden`}>
+
+          {gen.generatedImage && (
+            <img
+              src={gen.generatedImage}
+              alt={gen.productName}
+              className={`absolute inset-0 w-full h-full object-cover transition duration-500 ${gen.generatedVideo ? 'group-hover:opacity-0' : 'group-hover:scale-105'}`}
+            />
+          )}
+
+          {gen.generatedVideo && (
+            <video
+              src={gen.generatedVideo}
+              muted
+              loop
+              playsInline
+              className="absolute inset-0 w-full h-full object-cover opacity-0 group-hover:opacity-100 transition duration-500"
+              onMouseEnter={(e) => e.currentTarget.play()}
+              onMouseLeave={(e) => e.currentTarget.pause()}
+            />
+          )}
+
+          {(!gen?.generatedImage && !gen?.generatedVideo) && (
+            <div className="absolute inset-0 w-full h-full flex flex-col items-center justify-center bg-black/20">
+              <Loader2Icon className="size-7 animate-spin" />
+            </div>
+          )}
+
+          <div className="absolute left-3 top-3 flex gap-2 items-center">
+            {gen.isGenerating && (
+              <span className="text-xs px-2 py-1 bg-yellow-600/30 rounded-full">
+                Generating
+              </span>
             )}
-
-            {gen.generatedVideo && (
-            <video src={gen.generatedVideo} muted loop playsInline className="absolute inset-0 w-full h-full object-cover opacity-0 group-hover:opacity-100 transition duration-500" onMouseEnter={(e)=>e.currentTarget.play()}
-            onMouseLeave={(e)=>e.currentTarget.pause()}/>
+            {gen.isPublished && (
+              <span className="text-xs px-2 py-1 bg-green-600/30 rounded-full">
+                Published
+              </span>
             )}
+          </div>
 
-            {(!gen?. generatedImage && !gen?. generatedVideo)&&
-            (<div className="absolute inset-0 w-full h-full flex flex-col items-center justify-center bg-black/20">
-                <Loader2Icon className="size-7 animate-spin"/>
-            </div>)
-            }
+          {/*Actions for creator*/}
+          {!forCommunity && (
+            <div className="absolute right-3 top-3 sm:opacity-0 group-hover:opacity-100 transition flex items-center gap-2"  >
+
+              <div className="relative">
+                <EllipsisIcon
+                  onClick={() => setMenuOpen((prev) => !prev)}
+                  className="ml-auto bg-black/10 rounded-full p-1 size-7 cursor-pointer"
+                />
+
+                <ul className={`absolute right-0 mt-2 text-xs ${menuOpen ? 'block' : 'hidden'} overflow-hidden w-40 bg-black/50 backdrop-blur text-white border border-gray-500/50 rounded-lg shadow-md py-1 z-10`}>
+                  {gen.generatedImage && (
+                  <a href={gen.generatedImage} download className="flex gap-2 items-center px-4 py-2 hover:bg-black/10 cursor-pointer">
+                    <ImageIcon size={14} />
+                    Download Image  
+                  </a>
+                  )}
+
+                  {gen.generatedVideo && (
+                  <a href={gen.generatedVideo} download className="flex gap-2 items-center px-4 py-2 hover:bg-black/10 cursor-pointer">
+                    <VideoIcon size={14} />
+                    Download Video  
+                  </a>
+                  )}
+
+                  {(gen. generatedVideo || gen. generatedImage) && (
+                    <button onClick={()=> navigator.share({url: gen.generatedVideo || gen.generatedImage, title: gen.productName, text: gen.productDescription})} className="w-full flex gap-2 items-center px-4 py-2 hover:bg-black/10 cursor-pointer">
+                      <Share2Icon size={14} />
+                      Share
+                    </button>
+                  )}
+  
+                  <button onClick={() => handleDelete(gen.id)} className="w-full flex gap-2 items-center px-4 py-2 hover:bg-red-950/10 text-red-400 cursor-pointer">
+                    <Trash2Icon size={14} />
+                    Delete
+                  </button>
+                </ul>
+              </div>
+
+            </div>
+          )}
+
+
+
+          {/* Source Images */}
+          <div className="absolute right-3 bottom-3">
+            <img
+              src={gen.uploadedImages[0]}
+              alt="product"
+              className="w-16 h-16 object-cover rounded-full animate-float"
+            />
+            <img
+              src={gen.uploadedImages[1]}
+              alt="model"
+              className="w-16 h-16 object-cover rounded-full animate-float -ml-8"
+              style={{ animationDelay: '3s' }}
+            />
+          </div>
+
         </div>
 
-        {/*preview image*/}
-        <div></div>
+        {/* Details */}
+        <div className="p-4">
+          <div className="flex items-start justify-between gap-4">
+            <div className="flex-1">
+              <h3 className="font-medium text-lg mb-1">
+                {gen.productName}
+              </h3>
 
-    </div>
+              <p className="text-sm text-gray-400">
+                Created: {new Date(gen.createdAt).toLocaleString()}
+              </p>
+
+              {gen.updatedAt && (
+                <p className="text-xs text-gray-500 mt-1">
+                  Updated: {new Date(gen.updatedAt).toLocaleString()}
+                </p>
+              )}
+            </div>
+
+            <div className="text-right">
+              <div className="mt-2 flex flex-col items-end gap-1">
+                <span className="text-xs px-2 py-1 bg-white/5 rounded-full">
+                  Aspect: {gen.aspectRatio}
+                </span>
+              </div>
+            </div>
+
+          </div>
+
+          {/* product description */} 
+          {gen.productDescription && (
+            <div className="mt-3">
+                <p className="text-xs text-gray-400 mb-1">Description</p>
+                <div className="text-sm text-gray-300 bg-white/3 p-2 rounded-md break-words">{gen.productDescription}</div>
+            </div>
+          )}
+
+          {/*User prompt*/}
+            {gen.userPrompt && (
+              <div className="mt-3">
+                {/* <p className="text-xs text-gray-400 mb-1">Prompt: </p> */}
+                <div className="text-sm text-gray-300">{gen.userPrompt}</div>
+              </div>
+            )}
+
+          {/* buttons */}
+        {!forCommunity && (
+          <div className="mt-4 grid grid-cols-2 gap-3">
+
+            <GhostButton
+              className="text-xs justify-center"
+              onClick={() => {
+                navigate(`/result/${gen.id}`)
+                scrollTo(0, 0)
+              }}
+            >
+              View Details
+            </GhostButton>
+
+            <PrimaryButton
+              onClick={() => togglePublish(gen.id)}
+              className="rounded-md"
+            >
+              {gen.isPublished ? 'Unpublish' : 'Publish'}
+            </PrimaryButton>
+
+          </div>
+        )}
+
+
+
+
+        </div>
+
+      </div>
     </div>
   )
 }
 
 export default ProjectCard
-
-
-
-
